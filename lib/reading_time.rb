35673060e14fce158ad9702778a48a3547e3d9a0
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-02-20 16:14:28 +0000

    Collab with Monica reading time exercise

---
+++ lib/reading_time.rb
@@ -0,0 +1,7 @@
+#Collabrated on with Monica
+def reading_time(text)
+  word_per_second = 200/60.to_f
+  text_arr = text.split(" ")
+  time_read_in_s = text_arr.length * word_per_second
+  return "Reading time = #{time_read_in_s}s"
+end
---
+++ spec/reading_time_spec.rb
@@ -0,0 +1,11 @@
+# Collaborated with Monica
+require "reading_time"
+
+RSpec.describe "reading_time method" do
+    context "the method returns the amount of time to read a piece of text" do
+        it "returns the time" do
+            result = reading_time("Hello all how was your day")
+            expect(result).to eq("Reading time = 20.0s")
+        end
+    end
+end
