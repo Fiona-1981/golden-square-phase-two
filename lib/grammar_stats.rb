0a68803602e498e815a75245c4e9c1cbfdab5993
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-02-23 13:37:00 +0000

    Grammar stats hal way through challenge

--- lib/grammar_stats.rb
+++ lib/grammar_stats.rb
@@ -1,14 +1,22 @@
 class GrammarStats
   def initialize(text)
+    # ...
     @text = text
+    @percentage_good = percentage_good
   end
 
-  def check(text)
-    if text.start_with?('A'..'Z')
-      return true
+  def check(text) # text is a string
+    # Returns true or false depending on whether the text begins with a capital
+    # letter and ends with a sentence-ending punctuation mark.
+      if text.start_with?(/[A-Z]/) && text.end_with?(".")
+        return true
+      else
+        return false
     end
   end
 
-  # def percentage_good(percentage_good)
-    
+  def percentage_good
+    # Returns as an integer the percentage of texts checked so far that passed
+    # the check defined in the `check` method. The number 55 represents 55%.
   end
+end
--- spec/grammar_stats_spec.rb
+++ spec/grammar_stats_spec.rb
@@ -1,8 +1,23 @@
 require "grammar_stats"
 
 describe GrammarStats do
-  it "returns true or false if string starts with a capital" do
-  grammar_stats = GrammarStats.new("A not sloppy sentence.")
-  expect(grammar_stats.check).to eq true
+  it "checks for empty string" do
+    grammar_stats = GrammarStats.new("")
+    expect(grammar_stats.check("")).to eq false
+  end
+
+  it "checks for capitals" do
+    grammar_stats = GrammarStats.new("A sentence.")
+    expect(grammar_stats.check("A sentence.")).to eq true
+  end
+
+  it "checks for suitable punctuation" do
+    grammar_stats = GrammarStats.new("A short sentence.")
+    expect(grammar_stats.check("A short sentence.")).to eq true
+  end
+
+  it "checks for lower case starts" do
+    grammar_stats = GrammarStats.new("a sloppy sentence.")
+    expect(grammar_stats.check("a sloppy sentence.")).to eq false
   end
 end
