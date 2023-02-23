d16904bd72ca360795e1089bc15b2f8f8737f066
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-02-23 16:55:11 +0000

    Completion of grammar stats

--- lib/grammar_stats.rb
+++ lib/grammar_stats.rb
@@ -1,22 +1,33 @@
+# Collab with Dilan, or rather, help from Dilan!
 class GrammarStats
-  def initialize(text)
+  def initialize
     # ...
-    @text = text
-    @percentage_good = percentage_good
+    @check = 0
+    @true = 0
+    @false = 0
   end
 
-  def check(text) # text is a string
+  def check(text) 
+    @check += 1
+    # text is a string
     # Returns true or false depending on whether the text begins with a capital
     # letter and ends with a sentence-ending punctuation mark.
-      if text.start_with?(/[A-Z]/) && text.end_with?(".")
+      if text.start_with?(/[A-Z]/) && text.end_with?(".", "!")
+        # /[[:punct:]]/
+        @true += 1
         return true
       else
+        @false += 1
         return false
     end
   end
 
+  
   def percentage_good
+    fail "Nothing has been entered." if @check == 0
     # Returns as an integer the percentage of texts checked so far that passed
     # the check defined in the `check` method. The number 55 represents 55%.
+    
+    return ((@true.to_f / @check) * 100).to_i
   end
 end
--- spec/grammar_stats_spec.rb
+++ spec/grammar_stats_spec.rb
@@ -1,23 +1,44 @@
+# Collab with Dilan, or rather, help from Dilan!
 require "grammar_stats"
 
 describe GrammarStats do
   it "checks for empty string" do
-    grammar_stats = GrammarStats.new("")
+    grammar_stats = GrammarStats.new
     expect(grammar_stats.check("")).to eq false
   end
 
   it "checks for capitals" do
-    grammar_stats = GrammarStats.new("A sentence.")
+    grammar_stats = GrammarStats.new
     expect(grammar_stats.check("A sentence.")).to eq true
   end
 
   it "checks for suitable punctuation" do
-    grammar_stats = GrammarStats.new("A short sentence.")
-    expect(grammar_stats.check("A short sentence.")).to eq true
+    grammar_stats = GrammarStats.new
+    expect(grammar_stats.check("A short sentence")).to eq false
   end
 
   it "checks for lower case starts" do
-    grammar_stats = GrammarStats.new("a sloppy sentence.")
+    grammar_stats = GrammarStats.new
     expect(grammar_stats.check("a sloppy sentence.")).to eq false
   end
+
+  it "checks if any text has been checked" do
+    grammar_stats = GrammarStats.new
+    expect { grammar_stats.percentage_good }.to raise_error "Nothing has been entered."
+  end
+
+  it "counts the number of true results" do
+    grammar_stats = GrammarStats.new
+    grammar_stats.check("A sentence.")
+    expect(grammar_stats.percentage_good).to eq 100
+  end
+
+  it "counts the number of true results" do
+    grammar_stats = GrammarStats.new
+    grammar_stats.check("A sentence.")
+    grammar_stats.check("A sentence!")
+    grammar_stats.check("A sentence")
+    expect(grammar_stats.percentage_good).to eq 66
+  end
+  
 end
